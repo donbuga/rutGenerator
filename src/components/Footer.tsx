bfd5dc16faa29d19a97d11e06f2b06cfa82ebbee
donbuga <bugadj@gmail.com>
2025-07-11 10:14:17 -0400

    Add link to Fake JSON generator in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,15 @@ import React from 'react';
 const Footer: React.FC = () => (
   <footer className="bg-gray-200 text-center py-4 mt-6">
     <p className="text-sm text-gray-600">Generador de RUTs &copy; 2025 / bugaDev</p>
+    <p className="text-sm text-gray-600">
+      Otras utilidades:{' '}
+      <a
+        href="https://donbuga.github.io/fake-json-generator/"
+        className="text-blue-500 hover:underline"
+      >
+        Fake json generator
+      </a>
+    </p>
   </footer>
 );
 
